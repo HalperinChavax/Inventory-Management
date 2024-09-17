49034f5908de64403d402ccedea657189c6a7a50
Chava Halperin <104844606+HalperinChavax@users.noreply.github.com>
2024-09-17 22:48:43 +0300

    Update Product.py
    .

--- Product.py
+++ Product.py
@@ -1,1 +1,5 @@
-class Product:    def __init__(self, name: str, price: float):        self.name = name        self.price = price
+
+class Product:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
