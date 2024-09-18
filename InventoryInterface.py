06ba307f7114dd2fca16626683e920a023ec9a5f
Halperin, ChavaX <chavax.halperin@intel.com>
2024-09-18 17:26:44 +0300

    add interface inventory,and improve

---
+++ InventoryInterface.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from Product import Product
+
+class InventoryInterface(ABC):
+
+    @abstractmethod
+    def add_product(self, product: Product) -> None:
+        pass
+
+    @abstractmethod
+    def remove_product(self, product_name: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_product(self, product_name: str) -> Product:
+        pass
+
+    @abstractmethod
+    def get_total_inventory_value(self) -> float:
+        pass
--- test_inventory_management.py
+++ test_inventory_management.py
@@ -6,34 +6,31 @@ from Product import Product
 def cart():
     return Inventory()
 
-def test_add_product(cart):
-    prod = Product("Cherry", 10)
-    cart.add_product(prod)
+@pytest.fixture
+def product():
+    return Product("Cherry", 10.0)
+
+def test_add_product(cart, product):
+    cart.add_product(product)
     assert len(cart.products) == 1
 
-def test_when_product_added_then_cart_contains_product(cart):
-    prod = Product("Cherry", 10)
-    cart.add_product(prod)
-    assert cart.get_product(prod.name) == prod
+def test_get_product(cart, product):
+    cart.add_product(product)
+    assert cart.get_product("Cherry") == product
 
-def test_remove_product(cart):
-    prod = Product("Cherry", 10)
-    cart.add_product(prod)
+def test_remove_product(cart, product):
+    cart.add_product(product)
     len_before_remove = len(cart.products)
-    print(len_before_remove)
     cart.remove_product("Cherry")
-    assert len(cart.products) < len_before_remove
-
-def test_get_product(cart):
-    prod = Product("Cherry", 10)
-    cart.add_product(prod)
-    assert cart.get_product("Cherry") == prod
+    assert len(cart.products) == len_before_remove - 1
 
 def test_get_total_inventory_value(cart):
     prod1 = Product("Apple", 5.0)
     prod2 = Product("Banana", 2.1)
     prod3 = Product("Cherry", 10.0)
+
     cart.add_product(prod1)
     cart.add_product(prod2)
     cart.add_product(prod3)
+
     assert cart.get_total_inventory_value() == 17.1
